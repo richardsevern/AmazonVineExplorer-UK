4abf72a80c97e8f49644eba363bf3f6194864ab6
richardsevern <84574727+richardsevern@users.noreply.github.com>
2023-12-18 23:34:58 +0000

    Update VineExplorer.user.js

--- VineExplorer.user.js
+++ VineExplorer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Amazon Vine Explorer
 // @namespace    http://tampermonkey.net/
-// @version      0.10.3.2
+// @version      0.10.3.2.1
 // @updateURL    https://raw.githubusercontent.com/richardsevern/AmazonVineExplorer-uk/main/VineExplorer.user.js
 // @downloadURL  https://raw.githubusercontent.com/richardsevern/AmazonVineExplorer-uk/main/VineExplorer.user.js
 // @description  Better View, Search and Explore for Amazon Vine Products - Vine Voices Edition
